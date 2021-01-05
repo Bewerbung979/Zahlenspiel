9fb7ebe8953acaf909222d60720e520ddac5f7ac
Bewerbung979 <77019658+Bewerbung979@users.noreply.github.com>
2021-01-05 21:25:03 +0100

    Add files via upload

---
+++ Zahlenratespiel.py
@@ -0,0 +1,23 @@
+import random 
+
+versuch = "" #Zahl die vom Spieler geraten wird
+versuch_anzahl = 0 #Anzahl der Versuche
+gesuchte_zahl = random.randint(0, 100) #Zufallszahl die erraten werden soll
+erraten = False #Zustand ob die Zahl erraten wurde
+
+
+print("Bitte raten sie eine Zahl von 0 bis 100: ")
+while erraten == False:
+    versuch_anzahl += 1 
+    versuch = int (input("Versuch: ")) #Benutzereingabe
+    
+    if versuch == gesuchte_zahl: #Zahl entspricht der gesuchten Zahl
+        print("Glückwunsch, du hast die Zahl in {} Versuchen erraten!".format(versuch_anzahl))
+        erraten = True #Schleife wird beendet
+        break
+
+    elif versuch > gesuchte_zahl:
+        print("Die gesuchte Zahl ist kleiner.")
+
+    else:
+        print("Die gesuchte Zahl ist größer.")
